88998b6018f92f1777c0512690984c88fc208a61
kaberege <kabgnestor@gmail.com>
2025-09-22 23:29:06 +0200

    feat: added allowed hosts domain

--- online_poll_system_backend/settings.py
+++ online_poll_system_backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['online-poll-system-backend.onrender.com']
+ALLOWED_HOSTS = ['kgn-online-poll-system-backend.onrender.com']
 
 
 # Application definition
